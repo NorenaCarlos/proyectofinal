6d70fd260b0446345c6dfc575ec4effceccf3d00
NorenaCarlos <carlos1997704@gmail.com>
2024-12-05 07:34:24 -0500

    cambios varios

---
+++ js/index.js
@@ -0,0 +1,4 @@
+// Ejemplo de interactividad: Mostrar alerta al hacer clic en "Más"
+document.querySelector('a[href="#mas"]').addEventListener('click', () => {
+  alert('¡Gracias por visitar nuestra sección "Más"!');
+});
